fc068a817fa7e0ad0401de77da5bf9856432820f
Vadim Veremeev <stigmatstyle@gmail.com>
2021-05-19 15:40:39 +0300

    initial commit #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import {Module} from '@nestjs/common'
 import {SequelizeModule} from "@nestjs/sequelize";
 import {UsersModule} from './users/users.module';
 import {ConfigModule} from "@nestjs/config";
-
+//tes
 @Module({
     controllers: [],
     providers: [],
